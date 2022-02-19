c95dd2b347599aa8521fcf9a4da63c14d2e927bc
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-19 10:55:51 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'aframe'
+import React, { Fragment } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import ScrollToTop from "./ScrollToTop";
+import Home from "./pages/Home";
