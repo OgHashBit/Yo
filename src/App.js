2b01c2f1a5511bb1498aee4e4a632129d12361e0
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-19 10:56:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,3 +9,32 @@ import {
 
 import ScrollToTop from "./ScrollToTop";
 import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Fragment>
+        <ScrollToTop />
+          <Switch>          
+            <Route exact path="/" component={Home} />
+            <Route path="/tutorials" component={TutorialsRoute} />
+            <Route path="/sandbox" component={Sandbox} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </>
+  );
+}
+
+function TutorialsRoute(props) {
+  return (
+    <>
+      <Route exact path={props.match.path} component={Tutorials} />
+      <Route path={`${props.match.path}/hello`} component={Hello} />      
+      <Route path={`${props.match.path}/bloch`} component={Bloch} />      
+    </>
+  )
+}
+
+export default App;
